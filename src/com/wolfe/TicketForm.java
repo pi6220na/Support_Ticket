3d8561bf4fa69ea0b6e79d846f168145df11a3bf
pi6220na <myemail@youremail.com>
2016-11-03 09:38:52 -0500

    Added GUI Form

---
+++ src/com/wolfe/TicketForm.java
@@ -0,0 +1,23 @@
+package com.wolfe;
+
+import javax.swing.*;
+
+/**
+ * Created by myrlin on 11/1/2016.
+ */
+public class TicketForm {
+    private JPanel rootPanel;
+    private JTable resolvedTicket;
+    private JTable openTicket;
+    private JTextField openTicketsTextField;
+    private JTextField resolvedTicketsTextField;
+    private JButton deleteTicketButton;
+    private JTextField reporterTF;
+    private JTextField descriptionTF;
+    private JButton addTicketButton;
+    private JComboBox priorityCB;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
